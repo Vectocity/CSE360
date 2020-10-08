a649df0d9759bda82f23d47f8d4d8f9308925e08
victo <victo@DESKTOP-OPC23JO>
2020-10-07 23:51:15 -0700

    Initial changes to AddingMachine

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,32 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String str = "0";
   
+  /*Constructor*/
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
-  
+  /* Get total*/
   public int getTotal () {
-    return 0;
+    return total;
   }
-  
+  /*return total added with value*/
   public void add (int value) {
+	  total = total + value;
+	  str += " + " + value;
   }
-
+  /*return total with value subtracted*/
   public void subtract (int value) {
+	  total = total - value;
+	  str += " - " + value;
   }
-
+  /*print toString*/
   public String toString () {
-    return "";
+    return str;
   }
-
+  /*clear*/
   public void clear() {
+	  total = 0;
   }
 }
